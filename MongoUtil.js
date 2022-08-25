9f3987b39f78d1c14c3e1d5225ba16ef9c819afe
Herlyn Mortensen <herlynmortensen@gmail.com>
2022-08-25 18:38:37 +0000

    Added start of the API

--- MongoUtil.js
+++ MongoUtil.js
@@ -1,12 +1,12 @@
 const MongoClient = require('mongodb').MongoClient;
 
-async function connect() {
-    const client = await MongoClient.connect("mongodb+srv://herlynmortensen:EBYB8LMXG4z3Kd3E@demo.m51ld.mongodb.net/?retryWrites=true&w=majority", {
+async function connect(mongoUri, databaseName) {
+    const client = await MongoClient.connect(mongoUri,{
         useUnifiedTopology: true
     })
 
-    //const db = client.db('restaurant_reviews');
-    const db = client.db('sample_airbnb');
+    const db = client.db(databaseName);
+    //const db = client.db('sample_airbnb');
     return db;
 }
 
--- index.js
+++ index.js
@@ -9,15 +9,31 @@ const app = express();
 app.use(express.json())
 app.use(cors());
 
+const MONGO_URI = "mongodb+srv://herlynmortensen:EBYB8LMXG4z3Kd3E@demo.m51ld.mongodb.net/?retryWrites=true&w=majority";
+const DB_NAME = "restaurant_reviews";
+
 async function main (){
-    const db = await mongoUtil.connect();
-    const reviews = await db.collection('listingsAndReviews').find({}).limit(10).toArray();
-    console.log(reviews);
+    const db = await mongoUtil.connect(MONGO_URI, DB_NAME);
+   
 
     app.get('/', function(req,res){
         res.json({
            'message':'I love candies and cupcakes'
         });
+       })
+
+    app.post('/reviews', async function(req,res){
+        await db.collection('reviews').insertOne({
+            "restaurant": "Les Amis",
+            "title": "Legendary Les Amis",
+            "cuisine": "French",
+            "review": "Been here many times and each time excellent and 3 michelin star worthy Each dish was exquisite and tasty. As expected, they go all out with the bread selection, amuse bouche, petit fours etc Service is down to earth and equally good.",
+            "ratings": 5,
+            
+        })
+        res.json({
+            'message':'ok'
+        })
        })
 }
 main();
